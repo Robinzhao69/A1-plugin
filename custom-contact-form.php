a1169aea67ec17ae58de575bf34598819c61deb0
Sander Bakkum <sanderbakkum@MacBook-Pro-van-Sander.local>
2024-05-13 09:05:40 +0200

    v1.1

--- custom-contact-form.php
+++ custom-contact-form.php
@@ -1,28 +1,32 @@
 <?php
 /*
-Plugin Name: Custom Contact Form Annemiek
+Plugin Name: Contact Form A1
 Description: A Contact Form for Annemiek ter Steeg
-Version: 1.0
+Version: 1.1
 Author: Sander Bakkum
 */
 
 // styles and scripts
-function custom_contact_form_scripts() {
-    
+function custom_contact_form_scripts()
+{
+    // Enqueue CSS file
+    wp_enqueue_style('custom-contact-form-style', plugin_dir_url(__FILE__) . 'style.css');
 }
 add_action('wp_enqueue_scripts', 'custom_contact_form_scripts');
 
 // Function to handle form submission
-function custom_contact_form_handle_submission() {
+function custom_contact_form_handle_submission()
+{
     if (isset($_POST['submit'])) {
-        // Sanitize inputs
+        
+        $name = sanitize_text_field($_POST['naam']);
         $email = sanitize_email($_POST['email']);
         $message = sanitize_text_field($_POST['message']);
 
         // Email parameters
-        $to = 'youremail@example.com'; // Change this to your email
+        $to = '31918@ma-web.nl'; 
         $subject = 'New Contact Form Submission';
-        $body = "Email: $email\n\nMessage: $message";
+        $body = "Naam:  $name <br><br> Email: $email <br><br> Message: $message";
         $headers = array('Content-Type: text/html; charset=UTF-8');
 
         // Send email
@@ -34,22 +38,23 @@ function custom_contact_form_handle_submission() {
 add_action('init', 'custom_contact_form_handle_submission');
 
 // Function to display the contact form
-function custom_contact_form() {
+function custom_contact_form()
+{
     ob_start();
-    ?>
-    <form method="post">
-        <label for="email">Email:</label>
-        <input type="email" name="email" id="email" required><br><br>
-        <label for="message">Bericht:</label>
-        <textarea name="message" id="message" required></textarea><br><br>
+?>
+    <form method="post" class="custom-contact-form">
+        <input type="text" name="naam" placeholder="Naam" required><br><br>
+        <input type="email" name="email" placeholder="Email" required><br><br>
+        <textarea name="message" placeholder="Vraag of opmerking" required></textarea><br><br>
         <input type="submit" name="submit" value="Submit">
     </form>
-    <?php
+<?php
     return ob_get_clean();
 }
 
 // Shortcode to display the contact form
-function custom_contact_form_shortcode() {
+function custom_contact_form_shortcode()
+{
     return custom_contact_form();
 }
 add_shortcode('custom_contact_form', 'custom_contact_form_shortcode');
